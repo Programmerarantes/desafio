bee6dc71d38a59acf7712b66b530a25fc46eee49
Matheus <125316650+Programmerarantes@users.noreply.github.com>
2024-02-19 17:51:08 -0300

    list name and cpf

--- Back/src/controllers/apiController.ts
+++ Back/src/controllers/apiController.ts
@@ -1,16 +1,48 @@
 import { Request, Response } from "express";
 import { Pessoas } from '../models/Pessoas'
+import { Op } from "sequelize";
 
 export const novoUsuario = async (req: Request, res: Response) => {
-    let {nome, cpf, rg, dataNascimento, sexo } = req.body
-    let newUser = await Pessoas.create({ nome, cpf, rg, dataNascimento, sexo })
-
-    res.status(201)
-    res.json({id: newUser.id, nome, cpf, rg, dataNascimento, sexo })
+    try {
+        let {nome, cpf, rg, dataNascimento, sexo } = req.body
+        let newUser = await Pessoas.create({ nome, cpf, rg, dataNascimento, sexo })
+        res.status(201)
+        res.json({id: newUser.id, nome, cpf, rg, dataNascimento, sexo })
+    } catch (error) {console.log(error)}
+    
 }
 
 export const listarUsuarios = async (req: Request, res: Response) => {
-    let list = await Pessoas.findAll()
-    
-    res.json({ list })
+    try {
+        let list = await Pessoas.findAll()
+        res.json({ list })
+    } catch (error) {console.log(error)}
+}
+
+export const listarNome = async (req: Request, res: Response) => {
+    try {
+        let {nome} = req.params
+        let user = await Pessoas.findOne({
+            where: {nome: { [Op.iLike]: `%${nome}%` }}
+        })
+        
+        res.json({user})
+
+    } catch (error) 
+    {console.log(error)}
+
+}
+
+export const listarCpf = async (req: Request, res: Response) => {
+    try {
+        let {cpf} = req.params
+        let user = await Pessoas.findOne({
+            where: {cpf}
+        })
+        
+        res.json({user})
+
+    } catch (error) 
+    {console.log(error)}
+
 }
--- Back/src/routes/api.ts
+++ Back/src/routes/api.ts
@@ -4,6 +4,8 @@ import * as ApiController from '../controllers/apiController'
 
 const router = Router()
 
+router.get('/pessoas/cpf/:cpf', ApiController.listarCpf)
+router.get('/pessoas/nome/:nome', ApiController.listarNome)
 router.get('/pessoas', ApiController.listarUsuarios)
 router.post('/pessoas', ApiController.novoUsuario)
 
